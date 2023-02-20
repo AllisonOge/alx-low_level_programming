417f5accf934562e70a510a9ab8c3496979682fe
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-20 14:47:45 -0800

    task 12 two 2-digits number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of 2 2-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i < 99; i++)
+		for (j = 1; j < 100; j++)
+		{
+			putchar('0' + (int)i / 10);
+			putchar('0' + i % 10);
+			putchar(' ');
+			putchar('0' + (int)j / 10);
+			putchar('0' + j % 10);
+
+			if (i <= 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+
+	return (0);
+}
