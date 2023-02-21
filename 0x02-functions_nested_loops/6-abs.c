3691f085bfa673dffc7d7317961117d8a50cd91b
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-21 08:04:33 -0800

    task 7 compute the absolute value of an interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: number to be computed
+ *
+ * Return: computed value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -33,7 +34,7 @@ int main(void)
 	_putchar(r + '0');
 	_putchar('\n'); */
 
-	int r;
+	/* int r;
 
 	r = print_sign(98);
 	_putchar(',');
@@ -54,8 +55,18 @@ int main(void)
 	_putchar(',');
 	_putchar(' ');
 	_putchar(r + '0');
-	_putchar('\n'); 
+	_putchar('\n'); */
 
+	int r;
 
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	
 	return (0);
 }
