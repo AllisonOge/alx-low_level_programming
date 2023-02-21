217aff2e7b5757a887ba94acd5b6008b3d02525a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-21 05:53:04 -0800

    task 1 print using outsourced functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char text[] = "_putchar\n";
+
+	unsigned long i;
+
+	for(i = 0; i < strlen(text); i++)
+	{
+		_putchar(text[i]);
+	}
+
+	return (0);
+}
