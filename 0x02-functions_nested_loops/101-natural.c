67a90707d22fc0e64ca8d8a28e704dbe0db6e63c
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-21 19:19:20 -0800

    task 14 program to compute the sum of multiples of 3 an d 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - computes the sum of the multiples of 3 and 5 up to 1024
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long total = 0;
+
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0)
+			total += i;
+		if (i % 5 == 0)
+			total += i;
+	}
+	printf("%ld\n", total);
+
+	return (0);
+}
