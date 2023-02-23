96d78c0452dfafe3f27c2fc05083536aeacbf2bc
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-23 05:38:39 -0800

    task 3 multiply two integers

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -8,17 +8,20 @@
  */
 int main(void)
 {
-    char c;
+    /*  char c; */
 
     /* c = 'A';
     printf("%c: %d\n", c, _isupper(c));
     c = 'a';
     printf("%c: %d\n", c, _isupper(c)); */
 
-    c = '0';
+    /* c = '0';
     printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+    printf("%c: %d\n", c, _isdigit(c)); */
+
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
 
     return (0);
 }
