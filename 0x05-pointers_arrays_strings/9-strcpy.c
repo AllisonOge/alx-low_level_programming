244f85fd1e3ca6b73eb387c88ab3fd8975f3d45a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-28 13:57:11 -0800

    task 10 copy string from src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: copied string
+ * @src: string to be copied
+ *
+ * Return: string copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
