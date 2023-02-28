3a98c9848f4d9bcc2705b0fc263ab5828e7a5e02
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-02-28 14:14:15 -0800

    task 11 convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: converted string
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
