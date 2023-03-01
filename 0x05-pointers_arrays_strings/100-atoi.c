691db19a89131dc263354e7f560ed79a383f6ae2
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-01 00:39:45 -0800

    task 11 correct output fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -9,5 +10,17 @@
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	char cleaned_string[256];
+
+	int i, j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (isdigit(s[i]) || (s[i] == '-' && j == 0))
+		{
+			cleaned_string[j] = s[i];
+			j++;
+		}
+	cleaned_string[j] = '\0';
+
+	return (atoi(cleaned_string));
 }
