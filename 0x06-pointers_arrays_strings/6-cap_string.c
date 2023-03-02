62173a26d91ae27c7dd83066914da2401a671fb6
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-02 15:16:17 -0800

    task 7 added separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <ctype.h>
+#include <string.h>
 
 /**
  * cap_string - capitalize all words of a string
@@ -13,8 +14,10 @@ char *cap_string(char *s)
 
 	int capitalize_next = 1;
 
+	char separators[] = " \t\n,.!?\"(){}";
+
 	for (; *p != '\0'; p++)
-		if (isspace(*p))
+		if (strchr(separators, *p) != NULL)
 			capitalize_next = 1;
 
 		else if (capitalize_next)
