f1947df2de9773d56bc6b69b0d42373c77e6aafd
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-01 23:03:25 -0800

    task 1 concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to result of src appended to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
