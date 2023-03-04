199d858622551b0b5e536be01e6142572ef2da2f
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-03-04 05:13:56 +0100

    formatting

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -22,124 +22,124 @@ void print_array(int *a, int n) {
  */
 int main(void)
 {
-    /* char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+        /* char s1[98] = "Hello ";
+        char s2[] = "World!\n";
+        char *ptr;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr); */
+        printf("%s\n", s1);
+        printf("%s", s2);
+        ptr = _strcat(s1, s2);
+        printf("%s", s1);
+        printf("%s", s2);
+        printf("%s", ptr); */
 
-    /* char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+        /* char s1[98] = "Hello ";
+        char s2[] = "World!\n";
+        char *ptr;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr); */
+        printf("%s\n", s1);
+        printf("%s", s2);
+        ptr = _strncat(s1, s2, 1);
+        printf("%s\n", s1);
+        printf("%s", s2);
+        printf("%s\n", ptr);
+        ptr = _strncat(s1, s2, 1024);
+        printf("%s", s1);
+        printf("%s", s2);
+        printf("%s", ptr); */
 
-    /* char s1[98];
-    char *ptr;
-    int i;
+        /* char s1[98];
+        char *ptr;
+        int i;
 
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
+        for (i = 0; i < 98 - 1; i++)
         {
-            printf(" ");
+                s1[i] = '*';
         }
-        if (!(i % 10) && i)
+        s1[i] = '\0';
+        printf("%s\n", s1);
+        ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+        printf("%s\n", s1);
+        printf("%s\n", ptr);
+        ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+        printf("%s", s1);
+        printf("%s", ptr);
+        for (i = 0; i < 98; i++)
         {
-            printf("\n");
+                if (i % 10)
+                {
+                printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                printf("\n");
+                }
+                printf("0x%02x", s1[i]);
         }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n"); */
+        printf("\n"); */
 
-    /* char s1[] = "Hello";
-    char s2[] = "World!";
+        /* char s1[] = "Hello";
+        char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1)); */
+        printf("%d\n", _strcmp(s1, s2));
+        printf("%d\n", _strcmp(s2, s1));
+        printf("%d\n", _strcmp(s1, s1)); */
 
-    /*int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+        /*int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int)); */
+        print_array(a, sizeof(a) / sizeof(int));
+        reverse_array(a, sizeof(a) / sizeof(int));
+        print_array(a, sizeof(a) / sizeof(int)); */
 
-    /*char str[] = "Look up!\n";
-    char *ptr;
+        /*char str[] = "Look up!\n";
+        char *ptr;
 
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str); */
+        ptr = string_toupper(str);
+        printf("%s", ptr);
+        printf("%s", str); */
 
-    /* char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
+        /* char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+        char *ptr;
 
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);*/
+        ptr = cap_string(str);
+        printf("%s", ptr);
+        printf("%s", str);*/
 
-    /* char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+        /* char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+        char *p;
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s); */
+        p = leet(s);
+        printf("%s", p);
+        printf("%s", s); */
 
-    /* char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-    char *p;
+        /* char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+        char *p;
 
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s); */
+        p = rot13(s);
+        printf("%s", p);
+        printf("------------------------------------\n");
+        printf("%s", s);
+        printf("------------------------------------\n");
+        p = rot13(s);
+        printf("%s", p);
+        printf("------------------------------------\n");
+        printf("%s", s);
+        printf("------------------------------------\n");
+        p = rot13(s);
+        printf("%s", p);
+        printf("------------------------------------\n");
+        printf("%s", s); */
 
-    /* print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n'); */
+        /* print_number(98);
+        _putchar('\n');
+        print_number(402);
+        _putchar('\n');
+        print_number(1024);
+        _putchar('\n');
+        print_number(0);
+        _putchar('\n');
+        print_number(-98);
+        _putchar('\n'); */
 
         char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
         char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
@@ -188,5 +188,5 @@ int main(void)
         {
                 printf("%s + %s = %s\n", n, m, res);
         }
-    return (0);
+        return (0);
 }
