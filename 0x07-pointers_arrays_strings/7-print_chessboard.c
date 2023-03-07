ff83eebfb85a82a72bb943bae106f89cb62ced50
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-06 16:27:33 -0800

    task 7 print a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2D array of char where each row represents a rank on a chessboard
+ * and each column represents a file
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+			printf("%c", a[row][col]);
+		printf("\n");
+	}
+}
