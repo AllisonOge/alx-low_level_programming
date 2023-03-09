1325220e448c52b9e07374ad6d821a18bec1f775
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-08 16:19:18 -0800

    task factorial of an integer

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -10,9 +10,19 @@ int main(void)
 {
 /*    _puts_recursion("Puts with recursion"); */
 /*    _print_rev_recursion("\nColton Walker"); */
-    int n;
+/*    int n;
 
     n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
+    printf("%d\n", n); */
+    int r;
+
+    r = factorial(1);
+    printf("%d\n", r);
+    r = factorial(5);
+    printf("%d\n", r);
+    r = factorial(10);
+    printf("%d\n", r);
+    r = factorial(-1024);
+    printf("%d\n", r);
     return (0);
 }
