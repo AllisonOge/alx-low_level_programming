f76373891a612bfee4fbcca03fafe6c32842882e
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-14 19:23:44 -0700

    task 3 seg fault fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
+
 	concat = (char *) malloc(strlen(s1) + strlen(s2) + 1);
+	if (concat == NULL)
+		return (NULL);
+
 	strcpy(concat, s1);
 	strcat(concat, s2);
 	return (concat);
