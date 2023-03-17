68442c3c5e0b07e848b0a5f64de8fd0bc80aa046
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-16 21:54:23 -0700

    task 3 requirement fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - allocate memory for an array using malloc
@@ -20,6 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
+	/* initialize all elements to 0 */
 	memset(arr, 0, nmemb * size);
 
 	return (arr);
