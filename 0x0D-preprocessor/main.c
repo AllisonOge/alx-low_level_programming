3b5abbb2d53438926d2fa77d3883a6bcfb365b1f
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-20 14:21:19 -0700

    task 5 function like macro for sum of two numbers

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -34,9 +34,28 @@
     printf("%.3f\n", a);
     return (0);
 } */
+/* #include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h" */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+/* int main(void)
+{
+    int i;
+    int j;
+
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
+} */
 #include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#include "4-sum.h"
+#include "4-sum.h"
 
 /**
  * main - check the code
@@ -45,11 +64,9 @@
  */
 int main(void)
 {
-    int i;
-    int j;
+    int s;
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
+    s = SUM(98, 1024);
+    printf("%d\n", s);
     return (0);
 }
