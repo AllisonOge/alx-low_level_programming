9aba73d0c0fb7bfc2de61f86689e22edb521e95a
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-20 18:55:30 -0700

    task 2 correct output fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,8 +18,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptrDog = malloc(sizeof(dog_t));
 	if (ptrDog == NULL)
 		return (NULL);
+
+	/* allocate memory for name and owner strings */
 	ptrDog->name = strdup(name);
+	if (ptrDog->name == NULL)
+	{
+		free(ptrDog);
+		return (NULL);
+	}
+
 	ptrDog->age = age;
+
 	ptrDog->owner = strdup(owner);
+	if (ptrDog->owner == NULL)
+	{
+		free(ptrDog->name);
+		free(ptrDog);
+		return (NULL);
+	}
+
 	return (ptrDog);
 }
