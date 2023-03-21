34216bf77e172954644d5fa3838019402bb7183f
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-20 18:44:54 -0700

    task 2 correct output fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
+#include <math.h>
+#include <ctype.h>
+
+/**
+ * printString - print a string
+ * @s: pointer to string
+ */
+void printString(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+}
 
 /**
  * main - multiplies two positive numbers
@@ -11,30 +27,30 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc >= 2)
+	int i;
+
+	if (argc >= 3)
 	{
-		int d1, d2, i, j, carry, len1, len2, product, pos;
+		int d1, d2, len1, len2, j, carry, product, pos;
 		bool error = false;
-		char *num1, *num2;
-		unsigned long long result;
+		char *num1, *num2, *message;
+		unsigned long result = 0;
 
 		num1 = argv[1];
 		num2 = argv[2];
 		len1 = strlen(num1);
 		len2 = strlen(num2);
-
-		/* iterate through each digit of first digit from right to left */
+		/* iterate through each digit of num1 from right to left */
 		for (i = 0; i < len1; i++)
 		{
-			if (atoi(num1[i]) >= 0)
+			if (isdigit(num1[len1 - i - 1]))
 			{
 				d1 = num1[len1 - 1 - i] - '0';
 				carry = 0;
-
 				/* iterate through each digit of num2 form right to left */
 				for (j = 0; j < len2; j++)
 				{
-					if (atoi(num2[j]) >= 0)
+					if (isdigit(num2[len2 - 1 - j]))
 					{
 						d2 = num2[len2 - 1 - j] - '0';
 						/* multiply the digits and add carry */
@@ -51,7 +67,6 @@ int main(int argc, char *argv[])
 						break;
 					}
 				}
-				
 				/* add the final carry to the result */
 				if (carry > 0)
 				{
@@ -67,23 +82,19 @@ int main(int argc, char *argv[])
 		}
 		if (error)
 		{
-			message = "Error\n";
-			for (i = 0; i < strlen(message); i++)
-				_putchar(message[i]);
+			printString("Error\n");
 			exit(98);
 		}
 		/* allocate memory for result */
 		message = malloc((sizeof(char) * i) + 1);
 
-		sprintf(message, "%d\n", result);
-		for (i = 0; i < strlen(message); i++)
-			_putchar(message[i]);
+		sprintf(message, "%lu\n", result);
+		printString(message);
+		free(message);
 	}
 	else
 	{
-		message = "Error\n";
-		for (i = 0; i < strlen(message); i++)
-			_putchar(message[i]);
+		printString("Error\n");
 		exit(98);
 	}
 	return (0);
--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initial a variable of type struct dog
@@ -10,6 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
 	d->name = strdup(name);
 	d->age = age;
 	d->owner = strdup(owner);
