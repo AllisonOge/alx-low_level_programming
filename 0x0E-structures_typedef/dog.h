855eb5324c6e7ed6fdf99060fe415264b2d363ce
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-20 17:21:30 -0700

    task 1 simple structure for a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure with the following props
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: assign a name, age and owner to a dog object
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
