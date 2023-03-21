c1d76300528f98f3f9ad34ffbb9e3f4cbebc377d
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-20 18:25:56 -0700

    task 6 free malloc for create function

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -30,9 +30,14 @@ int main(void)
     my_dog.age = 3.5;
     my_dog.owner = "Bob";
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age); */
+/*    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age); */
     dog_t *my_dog;
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
     return (0);
 }
