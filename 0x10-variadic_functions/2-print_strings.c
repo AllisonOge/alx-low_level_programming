51458424678d3d53ccc6004862e6751008646138
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-23 13:04:00 -0700

    task 3 print all strings passed to a function with delimiter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - print strings followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to func
+ * @...: strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	if (separator == NULL)
+		return;
+	/* start arg list */
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str == NULL)
+			str = "(nil)";
+		if (i == n - 1)
+			printf("%s", str);
+		else
+			printf("%s%s", str, separator);
+	}
+	printf("\n");
+	/* clean up */
+	va_end(ap);
+}
--- 0x10-variadic_functions/main.c
+++ 0x10-variadic_functions/main.c
@@ -14,6 +14,7 @@ int main(void)
     printf("%d\n", sum);
     sum = sum_them_all(4, 98, 1024, 402, -1024);
     printf("%d\n", sum); */
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+/*    print_numbers(", ", 4, 0, 98, -1024, 402); */
+    print_strings(", ", 2, "Jay", "Django");
     return (0);
 }
