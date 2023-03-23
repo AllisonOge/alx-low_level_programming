5f9d60ba9680b4beda5e15fb20290e7a71c53277
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-22 17:40:38 -0700

    task 4 arithmetic calculator correct output fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,6 +13,8 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
+	char *op;
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,17 +22,26 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (!get_op_func(argv[2]))
+	op = argv[2];
+	op_func = get_op_func(op);
+
+	if (op_func == NULL)
 	{
 		printf("Error\n");
-		exit(98);
-	} else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		result = get_op_func(argv[2])(num1, num2);
-		printf("%d\n", result);
+		exit(99);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	result = op_func(num1, num2);
+
+	if ((*op == '/' || *op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", result);
 	return (0);
 }
 
