82768b9014dc4cc3f49da68d306266457813814f
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-23 12:31:27 -0700

    task 2 print all numbers passed with delimiter

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints number followed by a new line
+ * @separator: delimiter for the numbers
+ * @n: number of integers to be passed to the fn
+ * @...: integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+	/* initialize the arg list */
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		/* iterate over arg in list */
+		if (i == n - 1)
+			printf("%d", va_arg(ap, int));
+		else
+			printf("%d%s", va_arg(ap, int), separator);
+	printf("\n");
+	/* clean up */
+	va_end(ap);
+}
