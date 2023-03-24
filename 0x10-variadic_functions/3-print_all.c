cbdc28b91eb902bde48d28a421b513407384f3ff
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-24 06:50:58 -0700

    task 4 ignore unsupported types

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,7 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 /**
  * print_all - prints anything (char, int, float, char *)
@@ -13,6 +14,7 @@ void print_all(const char * const format, ...)
 	va_list ap;
 	int i = 0;
 	char *s;
+	bool ignore = false;
 
 	va_start(ap, format);
 	while (format && i < (int) strlen(format))
@@ -21,16 +23,20 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
+				ignore = false;
 				printf("%c", va_arg(ap, int));
 				break;
 			case 'i':
+				ignore = false;
 				printf("%d", va_arg(ap, int));
 				break;
 			case 'f':
+				ignore = false;
 				printf("%f", va_arg(ap, double));
 				break;
 			case 's':
 			{
+				ignore = false;
 				s = va_arg(ap, char *);
 				if (s == NULL)
 					s = "(nil)";
@@ -39,10 +45,11 @@ void print_all(const char * const format, ...)
 			}
 			default:
 				/* ignore unsupported format specifiers */
+				ignore = true;
 				break;
 		}
 		/* print a comma after the argument, unless it's the last argument */
-		if (i != (int)strlen(format) - 1)
+		if (i != (int)strlen(format) - 1 && !ignore)
 			printf(", ");
 		i++;
 	}
