56508e44a9f97c5abb5a9c5f96230b20be5e0c75
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-30 04:11:00 -0700

    task 1 setup

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints the elements of a list_t list
+ * @h: linked list to be printed
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n_nodes = 0;
+	/* iterate over the linked list starting at the head */
+
+	return (n_nodes);
+}
