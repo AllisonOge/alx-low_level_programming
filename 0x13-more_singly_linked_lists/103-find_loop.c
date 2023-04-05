0526435222614ce8cc49ad330aa13d99b5712592
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-04-05 23:32:40 +0000

    task 15 using tortoise and hare algorithm

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
  * find_listint_loop - finds the loop in a linked list
@@ -9,31 +8,23 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	size_t hash;
-	listint_t *current = head;
-	/* create a set to store visted nodes */
-	size_t table_size = 1024;
-	size_t hash_mask = table_size - 1;
-	listint_t **hash_table = calloc(table_size, sizeof(*hash_table));
+	listint_t *slow = head, *fast = head;
 
-	if (hash_table == NULL)
-		exit(98);
-
-	while (current != NULL)
+	/* Check if loop exists in the linked list */
+	while (slow && fast && fast->next)
 	{
-		/* check if the node has been visited before */
-		hash = ((size_t) current) & hash_mask;
-		if (hash_table[hash] == current)
-		{
-			free(hash_table);
-			return (current);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast) {
+			/* Loop detected */
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow); /* Return node where the loop starts */
 		}
-		/* add node to table */
-		hash_table[hash] = current;
-
-		current = current->next;
 	}
-
-	free(hash_table);
-	return (NULL);
+	return (NULL); /* No loop detected */
 }
