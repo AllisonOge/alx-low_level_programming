9c63914e0214a33bdfee0a5f5ab6089f17221383
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-04-14 18:09:29 -0700

    task 3 correct output fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		n = write(fd, text_content, strlen(text_content));
 		if (n < 0)
