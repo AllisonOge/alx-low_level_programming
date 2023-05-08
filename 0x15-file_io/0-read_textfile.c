6d75d9bd6b92e4d240b0582261fe5c9981a55dde
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-05-08 23:12:20 +0000

    reverted untested code to first attempt :sob:

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,42 +9,56 @@
  * @filename: name of the file with content to be read
  * @letters: number of letters it should read and print
  *
+ * Description: we use a fixed-size buffer of BUFFER_SIZE bytes,
+ * read from the file using read() system call, which reads up to
+ * BUFFER_SIZE bytes at a time from the file and stores them in buf
+ * Then we check the number of bytes read against the remaining number
+ * of bytes to read (letters - n) and adjust it if necessary.
+ * We write the contents of buf to standard output using the write() system
+ * call and increment the total number of bytes read (n) by the number of
+ * bytes read. If we have read the required number of bytes (n == letters)
+ * we exit the loop. Finally, we close the file with the system call close()
+ * and return the total number of bytes.
+ *
+ * P.S. No handling of content with null characters (\0)
+ *
  * Return: total number of bytes read and printed if no errors otherwise
  * 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buf;
-	ssize_t n, bytes_read;
+	char buf[BUFFER_SIZE];
+	ssize_t n = 0, bytes_read, bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 
-    buf = malloc(letters + 1);
-	while ((bytes_read = read(fd, buf, strlen(buf))) != -1)
+	while (n < (ssize_t)letters && (bytes_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
-		n = write(STDOUT_FILENO, buf, bytes_read);
-		if (n == -1 || n != bytes_read)
+		if (n + bytes_read > (ssize_t)letters)
+			bytes_read = (ssize_t)letters - n;
+
+		bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+		if (bytes_written < 0 || bytes_written != bytes_read)
 		{
-            free(buf);
 			close(fd);
 			return (0);
 		}
+
+		n += bytes_read;
 	}
 
-	if (bytes_read == -1)
+	if (bytes_read < 0)
 	{
-        free(buf);
 		close(fd);
 		return (0);
 	}
 
-    free(buf);
 	close(fd);
 	return (n);
 }
