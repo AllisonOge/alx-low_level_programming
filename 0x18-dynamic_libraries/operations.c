c69a1bb9a1bd0e997cd91df6eeec962a7f59daab
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-06-19 17:55:20 -0700

    task 2 let us call c functions from Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+/* operations.c */
+
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - implements the sub operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - implements the mul operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - implements the div operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division of a by b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - implements the mod operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of the division of a by b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
