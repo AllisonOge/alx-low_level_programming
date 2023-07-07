6b19ae88e601ee6ae6290386411710a51087ed29
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-07-07 20:36:18 +0000

    task 4 get value of a given key from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hast table
+ * @key: key
+ *
+ * Return: value of the associated key or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int hash_index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[hash_index])
+		return (ht->array[hash_index]->value);
+	return (NULL);
+}
