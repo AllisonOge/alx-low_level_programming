bc1ef02229756db89ed18fa536be26154c406c54
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-07-07 21:50:20 +0000

    task 3 corrections to set function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * hash_table_set - add a element to the hash table
+ * hash_table_set - add or update an element to the hash table
  * @ht: hash table
  * @key: key
  * @value: value associated with the key
@@ -13,11 +13,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash_index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *item = create_item(key, value);
+	hash_node_t *item, *current;
 
+	if (ht == NULL)
+		return (0);
+	/* check if the key already exists in the hash table */
+	current = ht->array[hash_index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			/* update the value for the existing key */
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	/* create a new item */
+	item = create_item(key, value);
 	if (!item)
 		return (0);
-	/* add item to the beginning of hash table */
+	/* set the key and value for the new node */
+	item->key = strdup(key);
+	item->value = strdup(value);
+
+	/* add item to the beginning of list */
 	item->next = ht->array[hash_index];
 	ht->array[hash_index] = item;
 	return (1);
@@ -36,12 +58,13 @@ hash_node_t *create_item(const char *key, const char *value)
 
 	if (!item)
 		return (NULL);
-	if (key == NULL) /* key cannot be NULL */
+	if (key == NULL || *key == '\0') /* key cannot be NULL */
 	{
 		free(item);
 		return (NULL);
 	}
 	item->key = strdup(key);
+	item->value = NULL;
 	if (value)
 		item->value = strdup(value); /* handle null value for value */
 	return (item);
