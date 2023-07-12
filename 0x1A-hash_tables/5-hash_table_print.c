189d5ecb0102cb2527db4af243461164782574e2
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-07-12 15:08:58 -0700

    task 5 handles collision and proper formatting

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,14 +8,31 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	unsigned int ok = 0;
+	hash_node_t *current;
 
 	if (!ht)
 		return; /* if NULL print nothing */
 	printf("{");
 	for (i = 0; i < (ht->size - 1); i++)
+	{
 		if (ht->array[i])
-			printf("'%s': '%s',", ht->array[i]->key, ht->array[i]->value);
+		{
+			if (ok)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			ok = 1;
+			current = ht->array[i];
+			while (current->next)
+			{
+				current = current->next;
+				printf("%s': '%s'", current->key, current->value);
+				if (current->next)
+					printf(", ");
+			}
+		}
+	}
 	if (ht->array[i])
-		printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+		printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 	printf("}\n");
 }
