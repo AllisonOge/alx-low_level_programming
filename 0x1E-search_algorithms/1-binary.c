bb04e3229b1f1213b033fe64c8e3572b61b0da68
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-12-13 14:48:15 +0000

    task#1 changed implementation to iterative approach

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,39 +3,6 @@
 
 
 /**
- * binary_search_helper - Helper function for performing binary search
- * @array: pointer to array
- * @low: lowest index of subarray
- * @high: highest index of subarray
- * @value: value to search for
- *
- * Return: index where value is found otherwise -1
- */
-int binary_search_helper(int *array, int low, int high, int value)
-{
-	int i;
-	int mid = low + (high - low) / 2;
-
-	if (high >= low)
-	{
-		printf("Searching in array:");
-		for (i = low; i <= high; i++)
-		{
-			if (i < high)
-				printf("%d, ", array[i]);
-			if (i == high)
-				printf("%d\n", array[i]);
-		}
-		if (value == array[mid])
-			return (mid);
-		else if (value > array[mid])
-			return (binary_search_helper(array, mid + 1, high, value));
-		return (binary_search_helper(array, low, mid - 1, value));
-	}
-	return (-1);
-}
-
-/**
  * binary_search - function that searches for a value in a sorted array of
  * 		   integers using the binary search algorithm
  * @array: pointer to the first element of the array to search in
@@ -46,11 +13,32 @@ int binary_search_helper(int *array, int low, int high, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
+	int i;
 	int low = 0;
 	int high = (int)size - 1;
+	int mid;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	return (binary_search_helper(array, low, high, value));
+	while (high >= low)
+	{
+		mid = low + (high - low) / 2;
+		printf("Searching in array:");
+		for (i = low; i <= high; i++)
+		{
+			if (i < high)
+				printf("%d, ", array[i]);
+			if (i == high)
+				printf("%d\n", array[i]);
+		}
+
+		if (value == array[mid])
+			return (mid);
+		else if (value > array[mid])
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
 }
