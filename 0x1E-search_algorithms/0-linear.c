3d0c7d2585c4dc6557675698391ed2682447a8c0
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-12-13 14:56:18 +0000

    betty requirement

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
 
 /**
  * linear_search - a function that searches for a value in an array
- * 		   of integers using the linear search algorithm
+ * of integers using the linear search algorithm
  * @array: a pointer to the first element of the array
  * @size: number of elements in array
  * @value: value to search for
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,7 +4,7 @@
 
 /**
  * binary_search - function that searches for a value in a sorted array of
- * 		   integers using the binary search algorithm
+ * integers using the binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
